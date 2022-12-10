e9ecad0ca75ffd7511b70491bfe466c0e90611e8
ema <amir-hosseini@gmx.at>
2022-12-10 19:00:31 +0100

    the imageSwitcher in DescriptionFragment.kt is implemented

--- app/src/main/java/com/ema/dummyjson/view/DescriptionFragment.kt
+++ app/src/main/java/com/ema/dummyjson/view/DescriptionFragment.kt
@@ -1,12 +1,11 @@
 package com.ema.dummyjson.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.ema.dummyjson.R
 import com.ema.dummyjson.databinding.FragmentDescriptionBinding
 import com.squareup.picasso.Picasso
 
@@ -14,11 +13,8 @@ import com.squareup.picasso.Picasso
 class DescriptionFragment : Fragment() {
     lateinit var binding: FragmentDescriptionBinding
     private val args: DescriptionFragmentArgs by navArgs()
+    var currentIndex: Int = 0
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,18 +27,39 @@ class DescriptionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getTheValuesFromProductFragment()
+        setupImageSwitcher()
     }
 
-    private fun getTheValuesFromProductFragment(){
+    private fun getTheValuesFromProductFragment() {
         binding.tvDescription.text = args.description
-        binding.tvPrice.text=args.price.toString()
+        binding.tvPrice.text = args.price.toString()
         binding.tvBrand.text = args.brnad
         binding.rbRating.rating = args.rating
         binding.tvDiscount.text = args.discount.toString()
-        binding.ivImage
-        Picasso.get().load(args.image[0]).into(binding.ivImage)
+        binding.ivSwitcherImage
+        Picasso.get().load(args.image[0]).into(binding.ivSwitcherImage)
+    }
 
 
+   private fun setupImageSwitcher(){
+
+        binding.ivForward.setOnClickListener {
+            currentIndex++
+            if(currentIndex==args.image.size){
+                currentIndex=0
+            }
+            Picasso.get().load(args.image[currentIndex]).into(binding.ivSwitcherImage)
+        }
+
+        binding.ivBack.setOnClickListener {
+            currentIndex--
+            if (currentIndex == -1) {
+                currentIndex = args.image.size - 1
+            }
+            val imageForChange = args.image[currentIndex]
+            Picasso.get().load(imageForChange).into(binding.ivSwitcherImage)
+
+        }
     }
 
 
--- app/src/main/java/com/ema/dummyjson/view/ProductsAdapter.kt
+++ app/src/main/java/com/ema/dummyjson/view/ProductsAdapter.kt
@@ -34,35 +34,7 @@ class ProductsAdapter(var context: Context, var productList: MutableList<Product
         holder.tvTitle.text = product.title
         Picasso.get().load(product.images[0]).into(holder.iv)
 
-        val imagesIterator = product.images.listIterator()
 
-        var iForward = 1
-        holder.ivForward.setOnClickListener {
-            when (iForward) {
-                in 1 until (product.images.size) -> {
-                    holder.iv.tag = product.images[iForward]
-                    Picasso.get().load(product.images[iForward]).into(holder.iv)
-                    iForward++
-                    if (iForward == product.images.size) {
-                        iForward = 0
-                    }
-                }
-            }
-        }
-
-        var iBack = iForward
-        holder.ivBack.setOnClickListener {
-            when (iBack) {
-                in iForward..0 -> {
-                    holder.iv.tag = product.images[iBack]
-                    Picasso.get().load(product.images[iBack]).into(holder.iv)
-                    iBack--
-                    if (iBack == product.images.size) {
-                        iBack = product.images.size
-                    }
-                }
-            }
-        }
         holder.container.setOnClickListener {
             navigateAndSendDataToDescriptionFragment(product.brand,product.description,product.discountPercentage,product.price,product.rating,product.images,it)
         }
@@ -97,15 +69,11 @@ class ProductsAdapter(var context: Context, var productList: MutableList<Product
         val tvTitle: AppCompatTextView
         val tvPrice: AppCompatTextView
         val iv: AppCompatImageView
-        val ivBack: AppCompatImageView
-        val ivForward: AppCompatImageView
 
         init {
             tvTitle = itemView.findViewById(R.id.tv_title_item_products)
             tvPrice = itemView.findViewById(R.id.tv_price_item_products)
             iv = itemView.findViewById(R.id.iv_item_products)
-            ivBack = itemView.findViewById(R.id.iv_back_item_products)
-            ivForward = itemView.findViewById(R.id.iv_forward_item_products)
             container = itemView.findViewById(R.id.constraint_container_item)
         }
     }
